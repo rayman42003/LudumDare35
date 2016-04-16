671a3ca79c0085dbcac9894a210927771fa9b2e0
Richmond Chang <changrichmond@gmail.com>
2016-04-15 21:27:52 -0700

    Added game project

---
+++ desktop/src/com/changrichmond/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.changrichmond.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.changrichmond.game.ShapeshiftGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ShapeshiftGame(), config);
+	}
+}
---
+++ html/src/com/changrichmond/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.changrichmond.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.changrichmond.game.ShapeshiftGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new ShapeshiftGame();
+        }
+}
